320c35243eee1502c7eb153bc596e399333c8955
Holy Lovenia <holy.lovenia@gmail.com>
2017-04-24 13:16:43 +0700

    rapiin LoadingView

--- src/view/LoadingView.java
+++ src/view/LoadingView.java
@@ -3,6 +3,7 @@ package view;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 
 /**
@@ -20,32 +21,56 @@ public class LoadingView extends JFrame {
         loadingPanel.setLayout(loadingLayout);
         GridBagConstraints loadingConstraints = new GridBagConstraints();
         loadingConstraints.fill = GridBagConstraints.BOTH;
+        loadingPanel.add(createLoadingText(loadingLayout, loadingConstraints));
+        loadingPanel.add(createLoadingBacBac(loadingLayout, loadingConstraints));
+        loadingPanel.add(createLoadingGhost(loadingLayout, loadingConstraints, 0, "\\assets\\loading_blinky.gif"));
+        loadingPanel.add(createLoadingGhost(loadingLayout, loadingConstraints, 1, "\\assets\\loading_pinky.gif"));
+        loadingPanel.add(createLoadingGhost(loadingLayout, loadingConstraints, 2, "\\assets\\loading_inky.gif"));
+        loadingPanel.add(createLoadingGhost(loadingLayout, loadingConstraints, 3, "\\assets\\loading_clyde.gif"));
 
+        add(loadingPanel);
+        pack();
+    }
+
+    public JLabel createLoadingText(GridBagLayout loadingLayout, GridBagConstraints loadingConstraints) {
+        loadingConstraints.gridx = 0;
+        loadingConstraints.gridy = 0;
+        loadingConstraints.gridwidth = 4;
+        Font loadingFont = null;
+        try {
+            loadingFont = Font.createFont(Font.TRUETYPE_FONT, getClass().getResource("\\assets\\font\\game_over.ttf").openStream());
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JLabel loadingText = new JLabel("Loading...", JLabel.CENTER);
+        loadingText.setBorder(new EmptyBorder(20, 10, 10, 10));
+        loadingText.setFont(loadingFont.deriveFont(400f));
+        loadingText.setForeground(Color.WHITE);
+        loadingLayout.setConstraints(loadingText, loadingConstraints);
+        return loadingText;
+    }
+
+    public JLabel createLoadingBacBac(GridBagLayout loadingLayout, GridBagConstraints loadingConstraints) {
         loadingConstraints.gridx = 0;
         loadingConstraints.gridy = 1;
+        loadingConstraints.gridwidth = 4;
         URL sleeping_bacbac_path = getClass().getResource("\\assets\\loading.gif");
         JLabel sleepingBacbac = new JLabel(new ImageIcon(sleeping_bacbac_path), JLabel.CENTER);
         sleepingBacbac.setBorder(new EmptyBorder(10, 10, 40, 10));
         loadingLayout.setConstraints(sleepingBacbac, loadingConstraints);
-        loadingPanel.add(sleepingBacbac);
+        return sleepingBacbac;
+    }
 
-        loadingConstraints.gridx = 0;
-        loadingConstraints.gridy = 0;
-        JLabel loadingText = new JLabel("Loading...", JLabel.CENTER);
-        loadingText.setFont(new Font("Arial", Font.CENTER_BASELINE, 100));
-        loadingText.setForeground(Color.WHITE);
-        loadingText.setBorder(new EmptyBorder(90, 10, 10, 10));
-        loadingLayout.setConstraints(loadingText, loadingConstraints);
-        loadingPanel.add(loadingText);
-
-        loadingConstraints.gridx = 0;
+    public JLabel createLoadingGhost(GridBagLayout loadingLayout, GridBagConstraints loadingConstraints, int gridx, String image_path) {
+        loadingConstraints.gridx = gridx;
         loadingConstraints.gridy = 2;
-        URL loading_balls_path = getClass().getResource("\\assets\\loading-bar.gif");
-        JLabel loadingBalls = new JLabel(new ImageIcon(loading_balls_path), JLabel.CENTER);
-        loadingBalls.setBorder(new EmptyBorder(30, 10, 10, 10));
-        loadingLayout.setConstraints(loadingBalls, loadingConstraints);
-        loadingPanel.add(loadingBalls);
-        add(loadingPanel);
-        pack();
+        loadingConstraints.gridwidth = 1;
+        URL loading_ghost_path = getClass().getResource(image_path);
+        JLabel loadingGhost = new JLabel(new ImageIcon(loading_ghost_path), JLabel.CENTER);
+        loadingGhost.setBorder(new EmptyBorder(30, 10, 10, 10));
+        loadingLayout.setConstraints(loadingGhost, loadingConstraints);
+        return loadingGhost;
     }
 }
