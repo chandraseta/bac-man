801050e60b106862121dcb1da7992296c3ffbfc0
Felix Limanta <13515065@std.stei.itb.ac.id>
2017-04-26 00:26:55 +0700

    new VisibilityGraph()

--- src/controller/BacMan.java
+++ src/controller/BacMan.java
@@ -1,11 +1,16 @@
 package controller;
 
 import model.Arena;
+import model.VisibilityGraph;
 import model.character.Blinky;
 import model.character.Clyde;
 import model.character.Inky;
 import model.character.Pinky;
-import view.*;
+import view.CreditsView;
+import view.GameOverView;
+import view.GameView;
+import view.LoadingView;
+import view.TitleView;
 
 /**
  * Kelas berisi semua aspek game BacMan.
@@ -14,117 +19,118 @@ import view.*;
  */
 public class BacMan {
 
-    /**
-     * Tampilan permainan BacMan.
-     */
-    private GameView gameView;
+  /**
+   * Tampilan permainan BacMan.
+   */
+  private GameView gameView;
 
-    /**
-     * Controller untuk objek Player pada permainan.
-     */
-    private PlayerController bac;
+  /**
+   * Controller untuk objek Player pada permainan.
+   */
+  private PlayerController bac;
 
-    /**
-     * Arena permainan BacMan.
-     */
-    private static Arena gameArena;
+  /**
+   * Arena permainan BacMan.
+   */
+  private static Arena gameArena;
 
-    /**
-     * Controller untuk objek Ghost pada permainan.
-     */
-    private GhostController[] ghost;
+  /**
+   * Controller untuk objek Ghost pada permainan.
+   */
+  private GhostController[] ghost;
 
-    /**
-     * Score Player.
-     */
-    public static int score;
+  /**
+   * Score Player.
+   */
+  public static int score;
 
-    /**
-     * Penanda permainan sudah berakhir atau belum.
-     */
-    private static boolean gameEnd;
+  /**
+   * Penanda permainan sudah berakhir atau belum.
+   */
+  private static boolean gameEnd;
 
-    /**
-     * Mengembalikan score Player.
-     *
-     * @return score yang didapat Player.
-     */
-    public static int getScore() { return score; }
+  /**
+   * Mengembalikan score Player.
+   *
+   * @return score yang didapat Player.
+   */
+  public static int getScore() { return score; }
 
-    /**
-     * Mengembalikan apakah permainan berakhir atau tidak.
-     *
-     * @return gameEnd permainan sudah berakhir atau belum.
-     */
-    public static boolean isGameEnd() { return gameEnd; }
+  /**
+   * Mengembalikan apakah permainan berakhir atau tidak.
+   *
+   * @return gameEnd permainan sudah berakhir atau belum.
+   */
+  public static boolean isGameEnd() { return gameEnd; }
 
-    /**
-     * Menambahkan score Player sejumlah value.
-     *
-     * @param value nilai yang ditambahkan ke score Player.
-     */
-    public static void addScore(int value) {
-        score += value;
+  /**
+   * Menambahkan score Player sejumlah value.
+   *
+   * @param value nilai yang ditambahkan ke score Player.
+   */
+  public static void addScore(int value) {
+    score += value;
+  }
+
+  /**
+   * Program utama.
+   *
+   * @param args Command line argument.
+   */
+  public static void main(String[] args) {
+    gameEnd = false;
+    score = 0;
+    gameArena = new Arena();
+    new VisibilityGraph();
+    PlayerController bacman = new PlayerController();
+    GhostController[] ghostList = new GhostController[4];
+    ghostList[0] = new GhostController('a');
+    ghostList[1] = new GhostController('b');
+    ghostList[2] = new GhostController('c');
+    ghostList[3] = new GhostController('d');
+    TitleView titleView = new TitleView();
+    while (TitleView.visibility) {
+      System.out.println();
     }
-
-    /**
-     * Program utama.
-     *
-     * @param args Command line argument.
-     */
-    public static void main(String[] args) {
-        gameEnd = false;
-        score = 0;
-        gameArena = new Arena();
-        PlayerController bacman = new PlayerController();
-        GhostController[] ghostList = new GhostController[4];
-        ghostList[0] = new GhostController('a');
-        ghostList[1] = new GhostController('b');
-        ghostList[2] = new GhostController('c');
-        ghostList[3] = new GhostController('d');
-        TitleView titleView = new TitleView();
-        while (TitleView.visibility) {
-            System.out.println();
-        }
-        LoadingView loadingView = new LoadingView();
-        loadingView.setVisible(true);
-        GameView gameView = new GameView(bacman, (Blinky) ghostList[0].getGhost(),
-                (Inky) ghostList[2].getGhost(),
-                (Pinky) ghostList[1].getGhost(), (Clyde) ghostList[3].getGhost());
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        loadingView.setVisible(false);
-        loadingView.dispose();
-        gameView.setVisible(true);
-        bacman.start();
-        gameView.start();
-        for (int i = 0; i < 4; i++) {
-            ghostList[i].start();
-        }
-        while (!BacMan.isGameEnd()) {
-            bacman.run();
-            for (int i = 0; i < 4; i++) {
-                ghostList[i].run();
-            }
-            gameView.run();
-            gameEnd = (Arena.getCookieLeft() == 0);
-        }
-        GameOverView gameOverView = new GameOverView();
-        gameView.setVisible(false);
-        gameView.dispose();
-        titleView.dispose();
-        gameOverView.setVisible(true);
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        CreditsView creditsView = new CreditsView();
-        gameOverView.setVisible(false);
-        gameOverView.dispose();
-        creditsView.setVisible(true);
+    LoadingView loadingView = new LoadingView();
+    loadingView.setVisible(true);
+    GameView gameView = new GameView(bacman, (Blinky) ghostList[0].getGhost(),
+        (Inky) ghostList[2].getGhost(),
+        (Pinky) ghostList[1].getGhost(), (Clyde) ghostList[3].getGhost());
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+    loadingView.setVisible(false);
+    loadingView.dispose();
+    gameView.setVisible(true);
+    bacman.start();
+    gameView.start();
+    for (int i = 0; i < 4; i++) {
+      ghostList[i].start();
+    }
+    while (!BacMan.isGameEnd()) {
+      bacman.run();
+      for (int i = 0; i < 4; i++) {
+        ghostList[i].run();
+      }
+      gameView.run();
+      gameEnd = (Arena.getCookieLeft() == 0);
+    }
+    GameOverView gameOverView = new GameOverView();
+    gameView.setVisible(false);
+    gameView.dispose();
+    titleView.dispose();
+    gameOverView.setVisible(true);
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    CreditsView creditsView = new CreditsView();
+    gameOverView.setVisible(false);
+    gameOverView.dispose();
+    creditsView.setVisible(true);
+  }
 }
